b6f4871692a9ba6132d94a4e1cc672113411edf5
Jens Jansson <peppe@vaadin.com>
2019-02-01 09:55:35 +0200

    Updates

---
+++ precache-manifest.d2635fefe20670db4db12e363e2fa9b9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc99d155cfcfd966cc2dc0f6d44d316",
+    "url": "img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "46d7cb5bd7c44fc464f4",
+    "url": "main.css"
+  },
+  {
+    "revision": "0d645bdc85e359cedb84c1ae7fe2f74f",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "ab073ba059d1e990f93247eacbc4fba4",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "e044a63e034bf10304dad73138b8c74b",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "16a6e231db0f767f82644d8cfa072ba0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "149b0d29fbb618e8d4416f6b95fd8bf6",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "a9abd5531d9abb2652cb9f1a1c3322da",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "7db41f86b9b35ede092e5e7f19357f34",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "46d7cb5bd7c44fc464f4",
+    "url": "main.46d7cb5b.js"
+  },
+  {
+    "revision": "695302310558140e00fa1146a129711d",
+    "url": "index.html"
+  },
+  {
+    "revision": "9d88711c7ce6600a2359341f39dc720d",
+    "url": "img/icons/icon.pxm"
+  },
+  {
+    "revision": "f626a25858c6bc639e53577fab66c41d",
+    "url": "img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "e52339b69988fae403db5a178493587d",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "02f30cd18f29005cef61d37350aea604",
+    "url": "img/icons/favicon.png"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("precache-manifest.d2635fefe20670db4db12e363e2fa9b9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+
