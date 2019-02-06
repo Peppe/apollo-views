a84750e875a421501825d72e5e6afc8254db4aa7
Jens Jansson <peppe@vaadin.com>
2019-02-06 14:58:07 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-importScripts("precache-manifest.d59d719352c3e130b8122b4213e94638.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.cf46bb41a880607b4596a8d83dedbe03.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 
